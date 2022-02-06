dbcb4aa9950c26169472b9cc301501c18c4ee380
Hammouti Ali <ali.hammouti@epsi.fr>
2022-02-06 17:57:02 +0100

    feature : add close modal feature + toggle class "responsive"

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,28 +1,30 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-
 // DOM Elements
+const topnav = document.querySelector(".topnav");
 const burgerMenu = document.getElementById("burgerMenu");
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
-burgerMenu.addEventListener("click", e => {
-  editNav();
+// toggle class "responsive" onclick on the burger btn 
+burgerMenu.addEventListener("click", () => {
+  topnav.classList.toggle('responsive');
 });
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// hide modal form
+function hideModal() {
+  modalbg.style.display = "none";
+}
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch the hiden modal event
+closeBtn.addEventListener("click", hideModal);
