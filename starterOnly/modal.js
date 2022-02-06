32ea8d72c815be788a34f7de015b5cee3044cd41
Hammouti Ali <ali.hammouti@epsi.fr>
2022-02-06 04:22:56 +0100

    fix : do the responsive of all devices

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,22 +1,24 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function editNav() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
 
 
 // DOM Elements
-const burgerMenu = document.getElementById("burgerMenu");
+const topnav = document.querySelector(".topnav");
+const burgerMenu = document.querySelector(".icon");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-burgerMenu.addEventListener("click", e => {
-  editNav();
+burgerMenu.addEventListener("click", () => {
+  topnav.classList.toggle('responsive');
 });
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
