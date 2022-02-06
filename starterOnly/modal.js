40c57831db9392475fd6e63d15668be81d5453e1
Hammouti Ali <ali.hammouti@epsi.fr>
2022-02-06 05:11:18 +0100

    resolve error git

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,24 +1,22 @@
-// function editNav() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 
 // DOM Elements
-const topnav = document.querySelector(".topnav");
-const burgerMenu = document.querySelector(".icon");
+const burgerMenu = document.getElementById("burgerMenu");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-burgerMenu.addEventListener("click", () => {
-  topnav.classList.toggle('responsive');
+burgerMenu.addEventListener("click", e => {
+  editNav();
 });
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
